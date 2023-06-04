c44384e008c64e1b94e37e28aa98d66d2d818136
Jenne <125715755+M0N60@users.noreply.github.com>
2023-06-04 15:52:12 +0200

    Update bibliotheken.py

--- Aktuelle (Auf dem Pi)/Sensoren/main/bibliotheken.py
+++ Aktuelle (Auf dem Pi)/Sensoren/main/bibliotheken.py
@@ -1,24 +1,28 @@
 
 # generell fur Input und Outupt (PIN BELEGUNG) 
-# link von : https://www.elektronik-kompendium.de/sites/raspberry-pi/2611151.htm 
+# link von : https://pypi.org/project/RPi.GPIO/
 import RPi.GPIO as GPIO #sudo apt-get install python-rpi.gpio
 
 # bme bibioteken 
-# link von : https://core-electronics.com.au/guides/piicodev-atmospheric-sensor-bme280-raspberry-pi-guide/ 
+# link von : https://pypi.org/project/smbus2/ ++ https://pypi.org/project/RPi.bme280/
 import bme280
 import smbus2
 
-# ADC stuff (unsicher)
+# ADC bibioteken 
+# link von : https://joy-it.net/files/files/Produkte/COM-KY053ADC/COM-KY053ADC_Anleitung_2023-02-28.pdf
 import board
 import busio
 import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 
-# array stuff (gibt uns übergreifende Diagramm)
+# array 
+# link von : https://numpy.org/doc/stable/index.html
 import numpy as np
 
-# plotter stuff (ist zum plotten )
+# plotter 
+# link von : https://matplotlib.org/
 import matplotlib.pyplot as plt
 
 # time stuff (locale zeit und delay)
+# link von : https://docs.python.org/3/library/time.html
 import time
